bba6df2f36f18b96c929cbba7eb2c28cb78d79fd
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-04-09 20:17:19 +0300

    fix networking

--- .profile.d/cosmo_set_bspwm.sh
+++ .profile.d/cosmo_set_bspwm.sh
@@ -2,7 +2,7 @@
 export QT_QPA_PLATFORMTHEME=qt5ct
 export GTK_THEME=Tokyonight-Dark-BL
 export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
-if ls /sys/class/net/$DEFAULT_NETWORK_INTERFACE/wireless
+if [[ -d /sys/class/net/$DEFAULT_NETWORK_INTERFACE/wireless ]]
 then
 export DEFAULT_NETWORK_TYPE=wireless
 else

087a6f592db31f7cb97e1c33921147e54ec4e3de
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-04-10 00:28:23 +0300

    final change for the day

--- .profile.d/cosmo_set_bspwm.sh
+++ .profile.d/cosmo_set_bspwm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export QT_QPA_PLATFORMTHEME=qt5ct
 export GTK_THEME=Tokyonight-Dark-BL
-export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export DEFAULT_NETWORK_INTERFACE=$(nmcli device show | head -n1 | cut -d$' ' -f26)
 if [[ -d /sys/class/net/$DEFAULT_NETWORK_INTERFACE/wireless ]]
 then
 export DEFAULT_NETWORK_TYPE=wireless

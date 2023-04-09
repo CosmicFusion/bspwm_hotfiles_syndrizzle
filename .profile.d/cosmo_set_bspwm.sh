f83ccf3f1844e4db7e8d26bd493e1699eb9ec9e5
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-04-09 15:15:11 +0300

    fix networking

--- .profile.d/cosmo_set_bspwm.sh
+++ .profile.d/cosmo_set_bspwm.sh
@@ -8,3 +8,8 @@ export DEFAULT_NETWORK_TYPE=wireless
 else
 export DEFAULT_NETWORK_TYPE=wired
 fi
+
+export XDG_CURRENT_DESKTOP="ubuntu:pika:gnome:$XDG_CURRENT_DESKTOP" 
+export XDG_SESSION_DESKTOP="ubuntu:pika:gnome:$XDG_SESSION_DESKTOP" 
+export DESKTOP_SESSION="ubuntu:pika:gnome:$DESKTOP_SESSION"
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
